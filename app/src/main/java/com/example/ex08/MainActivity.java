3722a8f6470e6abfdfe023bea386d0d81c5244ba
eitan <eitanshiman@gmail.com>
2026-01-29 21:15:35 +0200

    second

--- app/src/main/java/com/example/ex08/MainActivity.java
+++ app/src/main/java/com/example/ex08/MainActivity.java
@@ -8,9 +8,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton button;
+    ImageView img;
+    int random = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        img = findViewById(R.id.img);
+    }
+
+    public void clicked(View view) {
+
+        random = (int) ((Math.random() * 3) + 1);
+
+        if (random == 1) {
+            img.setImageResource(R.drawable.img1); //
+            button.setImageResource(R.drawable.img4);
+        }
     }
 }
