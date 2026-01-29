152c926c0d5debe020d29946f9547b6513e049d6
eitan <eitanshiman@gmail.com>
2026-01-29 21:07:08 +0200

    First

---
+++ app/src/main/java/com/example/ex08/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ex08;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
